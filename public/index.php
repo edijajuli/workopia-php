bc603314dec3d97ba261b7e2014fed7b8c59b728
edijajuli <edijajuli1@gmail.com>
2024-11-02 23:35:34 +0700

    add a router to the project

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
 require "../helpers.php";
-loadView('home');
+$routes = [
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/404.php'
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+if (array_key_exists($uri, $routes)) {
+    require(basePath($routes[$uri]));
+} else {
+    require(basePath($routes['404']));
+}
