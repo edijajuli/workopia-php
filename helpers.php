fb515b87c242b61e230ffb22a8256d05e5c1df4d
edijajuli <edijajuli1@gmail.com>
2024-11-02 22:12:49 +0700

    add helpers.php for manage the import system

--- helpers.php
+++ helpers.php
@@ -1,14 +1,46 @@
 <?php
 
-
-
 /**
  * Get the base path
  * 
- * @param string
+ * @param string $path
  * @return string
  */
 function basePath($path = '')
 {
     return __DIR__ . '/' . $path;
+}
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadView($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View `{$name} not found`";
+    }
+}
+
+/**
+ * Load a partials
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadPartials($name)
+{
+    $partialsPath = basePath("views/partials/{$name}.php");
+
+    if (file_exists($partialsPath)) {
+        require $partialsPath;
+    } else {
+        echo "View `{$name} not found`";
+    }
 }
