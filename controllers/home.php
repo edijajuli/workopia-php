2ecd84ba1a9af64f510021f62f84dae1e5e42545
edijajuli <edijajuli1@gmail.com>
2024-11-03 21:31:46 +0700

    pass data to view

--- controllers/home.php
+++ controllers/home.php
@@ -1,2 +1,10 @@
 <?php
-loadView('home');
+
+$config = require basePath('config/db.php');
+$db = new Database($config);
+
+$listngs = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
+loadView('home', [
+    'listings' => $listngs
+]);
--- controllers/listings/index.php
+++ controllers/listings/index.php
@@ -1,2 +1,10 @@
 <?php
-loadView('listings/index');
+
+$config = require basePath('config/db.php');
+$db = new Database($config);
+
+$listngs = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
+loadView('listings/index', [
+    'listings' => $listngs
+]);
