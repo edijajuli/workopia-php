6d86288e3aab50fc5ae91b4c5d1ffedcfc8c78ee
edijajuli <edijajuli1@gmail.com>
2024-11-12 19:58:26 +0700

    add controller classes to home & listings

--- Framework/Router.php
+++ Framework/Router.php
@@ -10,16 +10,18 @@ class Router
      * Add new route
      * @param string $method
      * @param string $uri
-     * @param string $controller
+     * @param string $action
      * @return void
      */
 
-    public function regiterRoute($method, $uri, $controller)
+    public function regiterRoute($method, $uri, $action)
     {
+        list($controller, $controllerMethod) = explode('@', $action);
         $this->routes[] = [
             'method' => $method,
             'uri' => $uri,
-            'controller' => $controller
+            'controller' => $controller,
+            'controllerMethod' => $controllerMethod
         ];
     }
 
@@ -91,7 +93,13 @@ class Router
         foreach ($this->routes as $route) {
             if ($route) {
                 if ($route['uri'] === $uri && $route['method'] === $method) {
-                    require basePath('App/' . $route['controller']);
+                    //Extract controller and controller method
+                    $controller = 'App\\Controllers\\' . $route['controller'];
+                    $controllerMethod = $route['controllerMethod'];
+
+                    // Instantiate the controller and call the method
+                    $controllerInstance = new $controller();
+                    $controllerInstance->$controllerMethod();
                     return;
                 }
             }
