79f1e3b75fdba456cf8b3f412060a2816078f471
edijajuli <edijajuli1@gmail.com>
2024-11-21 00:18:18 +0700

    edit form

--- App/views/listings/edit.view.php
+++ App/views/listings/edit.view.php
@@ -5,7 +5,7 @@
 <!-- Post a Job Form Box -->
 <section class="flex justify-center items-center mt-20">
     <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-600 mx-6">
-        <h2 class="text-4xl text-center font-bold mb-4">Create Job Listing</h2>
+        <h2 class="text-4xl text-center font-bold mb-4">Edit Job Listing</h2>
         <?php if (isset($errors)) : ?>
             <?php foreach ($errors as $error) : ?>
                 <div class="message bg-red-100 p-3 my-3"><?= $error; ?></div>
@@ -75,7 +75,7 @@
             <button class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-3 rounded focus:outline-none">
                 Save
             </button>
-            <a href="/"
+            <a href="/listings/<?php echo $listing['id']; ?>"
                 class="block text-center w-full bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded focus:outline-none">
                 Cancel
             </a>
--- routes.php
+++ routes.php
@@ -3,18 +3,8 @@
 $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create');
+$router->get('listings/edit/{id}', 'ListingController@edit');
 $router->get('/listings/{id}', 'ListingController@show');
 $router->post('/listings', 'ListingController@store');
+$router->get('/listings/{id}', 'ListingController@update');
 $router->delete('listings/{id}', 'ListingController@destroy');
-
-// $router->get('/', 'controllers/home.php');
-// $router->get('/listings', 'controllers/listings/index.php');
-// $router->get('/listings/create', 'controllers/listings/create.php');
-// $router->get('/listing', 'controllers/listings/show.php');
-
-
-// return [
-//     '/' => 'controllers/home.php',
-//     '/listings' => 'controllers/listings/index.php',
-//     '/listings/create' => 'controllers/listings/create.php',
-// ];
